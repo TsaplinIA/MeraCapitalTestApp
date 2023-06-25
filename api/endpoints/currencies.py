93f9273eb531ed972d0668487c9ba2b4b076b98d
Tsaplin Ilya <tsaplin.ia@outlook.com>
2023-06-26 02:20:33 +0300

    #11 add GET(by idx) and POST

--- api/endpoints/currencies.py
+++ api/endpoints/currencies.py
@@ -19,6 +19,23 @@ async def get_currencies_view(currencies: list[Currency] = Depends(Currency.get_
     return currencies
 
 
-@currencies_router.get('/currency/{currency_idx}')
-async def get_currency_by_id_view(currency_idx: UUID):
-    pass
+@currencies_router.get(
+    '/currency/{currency_idx}',
+    summary="Get currency",
+    description="Get currency by idx(uuid)",
+    response_model=CurrencyModel,
+)
+async def get_currency_by_id_view(currency_idx: UUID, currency: Currency = Depends(Currency.get_currency_by_idx)):
+    if currency is None:
+        raise HTTPException(detail=f"Currency with idx {currency_idx} not found", status_code=404)
+    return currency
+
+
+@currencies_router.post(
+    '/currency',
+    summary="Create currency",
+    description="Create currency. Specify index_price_name and ticker",
+    response_model=CurrencyModel,
+)
+async def create_currency_view(currency: Currency = Depends(Currency.create_currency)):
+    return currency
