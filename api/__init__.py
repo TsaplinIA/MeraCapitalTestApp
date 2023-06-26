61a0c704905e867cf01ad361fefa44c148a6c9e9
Tsaplin Ilya <tsaplin.ia@outlook.com>
2023-06-26 12:52:14 +0300

    #11 small refactor

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ from api.endpoints.currencies import currencies_router
 from api.endpoints.pricestamps import pricestamps_router
 
 app = FastAPI(title="Mera Capital Test app")
+
 app.include_router(pricestamps_router, tags=['pricestamps'])
 app.include_router(currencies_router, tags=['currencies'])
 
