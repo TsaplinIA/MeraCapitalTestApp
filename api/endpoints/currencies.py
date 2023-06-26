6913eb1f60664d706e711d8f4917743647990301
Tsaplin Ilya <tsaplin.ia@outlook.com>
2023-06-26 12:53:17 +0300

    #11 delete currency_idx duplicate

--- api/endpoints/currencies.py
+++ api/endpoints/currencies.py
@@ -25,9 +25,9 @@ async def get_currencies_view(currencies: list[Currency] = Depends(Currency.get_
     description="Get currency by idx(uuid)",
     response_model=CurrencyModel,
 )
-async def get_currency_by_id_view(currency_idx: UUID, currency: Currency = Depends(Currency.get_currency_by_idx)):
+async def get_currency_by_id_view(currency: Currency = Depends(Currency.get_currency_by_idx)):
     if currency is None:
-        raise HTTPException(detail=f"Currency with idx {currency_idx} not found", status_code=404)
+        raise HTTPException(detail=f"Currency with this idx not found", status_code=404)
     return currency
 
 
