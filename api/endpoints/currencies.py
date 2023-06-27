1812a616eb1044e315c56e2a048bd9d05ab35e32
Tsaplin Ilya <tsaplin.ia@outlook.com>
2023-06-27 07:18:46 +0300

    #11 add PATCH and DELETE currencies

--- api/endpoints/currencies.py
+++ api/endpoints/currencies.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.models.currencies import CurrencyModel, CurrencyCreateModel
+from api.models.currencies import CurrencyModel, CurrencyCreateModel, CurrencyUpdateModel
 from database import get_session, Currency
 
 currencies_router = APIRouter()
@@ -41,5 +41,36 @@ async def create_currency_view(
         data: CurrencyCreateModel,
         session: AsyncSession = Depends(get_session),
 ):
-    currency = Currency.create_currency(data.ticker, data.index_price_name, session)
+    currency = await Currency.create_currency(data.ticker, data.index_price_name, session)
     return currency
+
+
+@currencies_router.patch(
+    '/currency/{currency_idx}',
+    summary="Update currency",
+    description="Update currency. Specify index_price_name and ticker",
+    response_model=CurrencyModel,
+)
+async def create_currency_view(
+        currency_idx: UUID,
+        data: CurrencyUpdateModel,
+        session: AsyncSession = Depends(get_session),
+):
+    currency = await Currency.update_currency_by_idx(currency_idx, data.ticker, data.index_price_name, session)
+    return currency
+
+
+@currencies_router.delete(
+    '/currency/{currency_idx}',
+    summary="Delete currency",
+    description="Delete currency by id",
+    response_model=str,
+)
+async def create_currency_view(
+        currency_idx: UUID,
+        session: AsyncSession = Depends(get_session),
+):
+    is_deleted = await Currency.delete_currency(currency_idx, session)
+    if not is_deleted:
+        raise HTTPException(detail="Currency not found", status_code=404)
+    return f"currency {currency_idx} has been deleted"
